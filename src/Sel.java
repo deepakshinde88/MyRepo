9b79f3937173de511e971b57741bb7d165444921
deepakshinde88 <edlshinde@gmail.com>
2017-06-16 00:21:01 +0530

    DeepkUpdateJune17

---
+++ src/Sel.java
@@ -0,0 +1,18 @@
+import org.apache.xpath.operations.String;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Sel{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new FirefoxDriver();
+driver.get("http://google.co.in");
+//get("http://google.co.in");
+	}
+
+}
